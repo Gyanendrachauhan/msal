8f790b0a74cd497c26bf3ae264e5ce4ff9ced746
Gyanendra Singh <gyanendra.singh@globtier.in>
2023-10-26 12:56:24 +0530

    first commit

---
+++ app_config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+CLIENT_ID = os.getenv("CLIENT_ID")
+
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+AUTHORITY = os.getenv("AUTHORITY")
+
+REDIRECT_PATH = "/webhook"  # Used for forming an absolute URL to your redirect URI.
+                              # The absolute URL must match the redirect URI you set
+                              # in the app's registration in the Azure portal.
+
+# You can find more Microsoft Graph API endpoints from Graph Explorer
+# https://developer.microsoft.com/en-us/graph/graph-explorer
+ENDPOINT = 'https://graph.microsoft.com/v1.0/users'  # This resource requires no admin consent
+
+# You can find the proper permission names from this document
+# https://docs.microsoft.com/en-us/graph/permissions-reference
+SCOPE = ["User.ReadBasic.All"]
+
+SESSION_TYPE = "filesystem"  # Specifies the token cache should be stored in server-side session
